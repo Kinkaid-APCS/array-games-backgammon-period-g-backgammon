b5449f38bc211da746f7bdc98c4af95fddce81ad
A-Person1243 <1adi.kejriwal@gmail.com>
2023-10-18 14:53:20 -0500

    worked on Referee class

--- src/Board.java
+++ src/Board.java
@@ -25,7 +25,6 @@ public class Board {
 	public Board()
 	{
 		//--------------------
-		// TODO: insert your code here.
 		
 		//--------------------
 	}
--- src/Referee.java
+++ src/Referee.java
@@ -8,13 +8,18 @@ public class Referee {
 	// TODO: decide which private member variables the Referee should have and declare them here.
 	// suggestion: the dice are an array of integers, typically 1-6, where 0 means unrolled or 
 	// used up already.
-	
-	
+	private int[] diceValues = {0, 1, 2, 3, 4, 5, 6};
+	public boolean continuePlaying = true;
+	DiceCup blackCup = new DiceCup();
+	DiceCup whiteCup = new DiceCup();
+	Board board = new Board();
 	/**
 	 * constructor - set up the board and players 
 	 */
 	// TODO: you write the Referee's constructor
-	
+	Referee() {
+
+	}
 	/**
 	 * playGame - the main game loop. Roll the dice, ask the user for a
 	 * move, determine whether it is legal, and then execute the move. 
@@ -23,6 +28,8 @@ public class Referee {
 	public void playGame()
 	{
 		// TODO: you write the Referee's playGame method.
-		System.out.println("Playing game."); // placeholder code.
+		while (continuePlaying == true) {
+
+		}
 	}
 }
